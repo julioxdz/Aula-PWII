9c412bc160c96b6a2bd5a541cf7a3ab21ac57f5a
julioxdz <162614996+julioxdz@users.noreply.github.com>
2024-08-23 13:31:14 -0300

    teste.php

--- loginETIM/teste.php
+++ loginETIM/teste.php
@@ -1,5 +1,11 @@
 <?php
 include 'Contato.class.php';
-
 $contato = new Contato();
 
+$contato-> setNome ("fabio");
+$contato-> setEmail ("fabioclaret@gmail.com");
+$contato-> setSenha ("123");
+
+echo "<h2>Nome: ".$contato->getNome()."<br>";
+echo "Email: ". $contato->getEmail()."<br>";
+echo "Senha: ". $contato->getSenha()."<br>";
